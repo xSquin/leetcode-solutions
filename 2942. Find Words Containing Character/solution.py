48f7948842212c05f01d199e21a3effaf7a16d2d
Igor Siemakowicz <116567088+xSquin@users.noreply.github.com>
2025-10-08 21:53:04 +0200

    Update solution.py

--- 2942. Find Words Containing Character/solution.py
+++ 2942. Find Words Containing Character/solution.py
@@ -1,1 +1,7 @@
-c
+class Solution(object):
+    def findWordsContaining(self, words, x):
+        result = []
+        for i in range(0, len(words)):
+            if x in words[i]:
+                result.append(i)
+        return result
