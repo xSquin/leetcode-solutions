c45928693fc29780aaa29b0c1fe2d86904ca8634
Igor Siemakowicz <116567088+xSquin@users.noreply.github.com>
2025-10-08 21:57:16 +0200

    Update solution.py

--- 1470. Shuffle the Array/solution.py
+++ 1470. Shuffle the Array/solution.py
@@ -1,1 +1,12 @@
-c
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        array = []
+        for i in range(0, len(nums)/2):
+            array.append(nums[i])
+            array.append(nums[i+n])
+        return array
