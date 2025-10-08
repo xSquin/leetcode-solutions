91e508d1b51dd862413dd49a941b33f0daf69524
Igor Siemakowicz <116567088+xSquin@users.noreply.github.com>
2025-10-08 22:07:54 +0200

    Add solution for 1816. Truncate Sentence

---
+++ 1816. Truncate Sentence/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def truncateSentence(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        text = " ".join(s.split()[:k])
+        return text
+        
