18c85a551e8522c841dd28875e39030007f21b8a
Igor Siemakowicz <116567088+xSquin@users.noreply.github.com>
2025-10-08 21:57:56 +0200

    Update solution.py

--- 75. Sort Colors/solution.py
+++ 75. Sort Colors/solution.py
@@ -1,1 +1,11 @@
-c
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            for j in range(0, len(nums) - 1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+        
