3380da479a79ebb58becca2e178ba8fcde2216bc
Igor Siemakowicz <116567088+xSquin@users.noreply.github.com>
2025-10-08 21:56:14 +0200

    Update solution.py

--- 1512. Number of Good Pairs/solution.py
+++ 1512. Number of Good Pairs/solution.py
@@ -1,1 +1,13 @@
-c
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        for i, i_word in enumerate(nums):
+            for j, j_word in enumerate(nums):
+                if i_word == j_word and i < j:
+                    result += 1
+        return result 
+        
