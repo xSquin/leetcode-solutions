9dbb0bcbf9639df511627cee3e39204fd7f457c1
Igor Siemakowicz <116567088+xSquin@users.noreply.github.com>
2025-10-10 22:44:01 +0200

    Update solution.py

--- 1768. Merge Strings Alternately/solution.py
+++ 1768. Merge Strings Alternately/solution.py
@@ -1,17 +1,23 @@
-        """
-        res = ""
-        shortest = ""
-        if len(word1) <= len(word2):
-            shortest = word1
-            longest = word2
-        longest = ""
-        else:
-            shortest = word2
-            longest = word1
-        for i in range(0, len(shortest)):
-            res += word1[i]
-            res += word2[i]
-            res += longest[-(len(longest) - len(shortest)):]
-        return res
-        if len(longest) != len(shortest):
-        
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        res = ""
+        shortest = ""
+        longest = ""
+        if len(word1) <= len(word2):
+            shortest = word1
+            longest = word2
+        else:
+            shortest = word2
+            longest = word1
+        for i in range(0, len(shortest)):
+            res += word1[i]
+            res += word2[i]
+        if len(longest) != len(shortest):
+            res += longest[-(len(longest) - len(shortest)):]
+        return res
+        
