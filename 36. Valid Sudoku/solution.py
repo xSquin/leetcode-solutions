db6739bfe2fd7f11afd134900d44e6eefd9e8c9d
Igor Siemakowicz <116567088+xSquin@users.noreply.github.com>
2025-10-14 18:33:32 +0200

    Add solution for 36. Valid Sudoku

---
+++ 36. Valid Sudoku/solution.py
@@ -0,0 +1,12 @@
+                        return False
+            return True
+
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j] == ".":
+                    pass
+                elif line_x(board[i][j], j, i) != True or line_y(board[i][j], j, i) != True or square3x3
+(board[i][j], j, i) != True:
+                    return False
+        return True
+        
