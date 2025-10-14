ffd1f47c444d86a17856133e13ff66b36764eca4
Igor Siemakowicz <116567088+xSquin@users.noreply.github.com>
2025-10-14 21:59:08 +0200

    Add solution for 36. Valid Sudoku

---
+++ 36. Valid Sudoku/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        def line_x(checked_num, x, y):
+            for i in range(len(board)):
+                if checked_num == board[y][i] and x != i:
+                    return False
