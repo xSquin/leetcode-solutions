9edf09f8b352bea6f5e43a95212ec4254379dccb
Igor Siemakowicz <116567088+xSquin@users.noreply.github.com>
2025-10-14 19:20:35 +0200

    Add solution for 36. Valid Sudoku

---
+++ 36. Valid Sudoku/solution.py
@@ -0,0 +1,17 @@
+                    if checked_num == board[i][j] and i != y and j != x:
+                        return False
+            return True
+                for j in range(x - (x%3), x - (x%3) + 3):
+            for i in range(y - (y%3), y - (y%3) + 3):
+            return True
+        def square3x3(checked_num, x, y):
+                    return False
+                if checked_num == board[i][x] and y != i:
+            for i in range(len(board)):
+        def line_y(checked_num, x, y):
+            return True
+                    return False
+                if checked_num == board[y][i] and x != i:
+            for i in range(len(board)):
+        def line_x(checked_num, x, y):
+        """
