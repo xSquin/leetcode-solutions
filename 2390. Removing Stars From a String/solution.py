3e93ae7e9610e20b65a58cb691af279c34a960c8
Igor Siemakowicz <116567088+xSquin@users.noreply.github.com>
2026-02-27 15:00:12 +0100

    Add solution for 2390. Removing Stars From a String

--- 2390. Removing Stars From a String/solution.py
+++ 2390. Removing Stars From a String/solution.py
@@ -1,15 +1,15 @@
-class Solution(object):
-    def removeStars(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        string = ""
-        for i in range(0, len(s)):
-            if s[i] != "*":
+class Solution(object):
+    def removeStars(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        string = ""
+        for i in range(0, len(s)):
+            if s[i] != "*":
+                string += s[i]
+            else:
+                string = string[:-1]
+        return string
 
-                string += s[i]
-            else:
-                string = string[:-1]
-        return string
-        
+        
